5c7464ba92077426c7b21e280f263c66b135fbbc
Elliedebian <ellie.debian@icloud.com>
2025-10-23 21:27:39 +1030

    Introduction Information
    description, author, ID, Username

--- Rig.py
+++ Rig.py
@@ -1,8 +1,8 @@
 """
 File: Rig.py
-Description: <A brief description of this Python module.>
-Author: <full name>
-ID: <student_id>
-Username: <username>
+Description: <Represents a hacker's computer system that stores digital assets, takes damage in battles, can be upgraded or repaired, and serves as the ,main tool for attacks and asset management.>
+Author: <Elham Debian>
+ID: <debey003>
+Username: <Elliedebian>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
