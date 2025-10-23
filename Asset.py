326d9f512d2399b0b67a72054a5a0a32c450694d
Elliedebian <ellie.debian@icloud.com>
2025-10-23 20:24:12 +1030

    def __init__
    Added constructor to set the name, description, and encryption flag for class.

--- Asset.py
+++ Asset.py
@@ -8,3 +8,9 @@ This is my own work as defined by the University's Academic Misconduct Policy.
 """
 
 class Asset:
+  def __init__(self, name, description, encrypted):
+      self.__name = name
+      self.__description = description
+      self.__encrypted = encrypted
+
+  
