48118149dffc4f15a67d1708452b2b0df71acbb9
Elliedebian <ellie.debian@icloud.com>
2025-10-23 21:49:55 +1030

    Rig class wth base attributes
    Created the Rig class with attributes for name, damage, broken status, upgrade level, and storage to define the core structure of a rig object.

--- Rig.py
+++ Rig.py
@@ -10,4 +10,11 @@ This is my own work as defined by the University's Academic Misconduct Policy.
 from asset import Asset
 
 class Rig:
+  def __init__(self, name):
+    self.__name = name
+    self.__damage = 0
+    self.__broken = False
+    self.__upgrade_level = 0
+    self.__storage = []
+
   
