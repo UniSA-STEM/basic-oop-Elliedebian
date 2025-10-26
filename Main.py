90fa12e647b419504d1a7e1dd42c05f779cc0d63
elliedebian <ellie.debian@icloud.com>
2025-10-26 14:53:04 +1030

    personal details and description

--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
 """
 File: main.py
-Description: <A brief description of this Python module.>
-Author: <full name>
-ID: <student_id>
-Username: <username>
+Description: <Runs the central game loop, coordinating hacker actions, rig interactions, and asset management to drive the overall hacking gameplay experience.>
+Author: <Elham Debian>
+ID: <debey003>
+Username: <Elliedebian>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
